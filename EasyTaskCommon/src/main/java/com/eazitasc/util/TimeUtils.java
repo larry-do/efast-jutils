c298f96c28aa7dece77910248001ed42a179dc77
Larry <annanshakai@gmail.com>
2021-07-19 09:55:48 +0700

    Update Utils

--- EasyTaskCommon/src/main/java/com/eazitasc/util/TimeUtils.java
+++ EasyTaskCommon/src/main/java/com/eazitasc/util/TimeUtils.java
@@ -10,6 +10,8 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class TimeUtils {
+    public static final long MILLISECONDS_PER_DAY = 86400000L;
+
     public static String DDHHMM = "ddHHmm";
     public static String DD_MM_YYYY_HH_MM_SS = "dd/MM/yyyy HH:mm:ss";
     public static String DD_MM_YYYY = "dd/MM/yyyy";
@@ -37,11 +39,27 @@ public class TimeUtils {
         return sdf.format(date);
     }
 
-    public static Date addMonth(Date date, int month) {
+    public static Date addTime(Date date, int field, int amount) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
-        calendar.add(Calendar.MONTH, month);
+        calendar.add(field, amount);
         return calendar.getTime();
+    }
+
+    public static Date addHour(Date date, int hour) {
+        return addTime(date, Calendar.HOUR, hour);
+    }
+
+    public static Date addDay(Date date, int day) {
+        return addTime(date, Calendar.DATE, day);
+    }
+
+    public static Date addMonth(Date date, int month) {
+        return addTime(date, Calendar.MONTH, month);
+    }
+
+    public static Date addYear(Date date, int year) {
+        return addTime(date, Calendar.YEAR, year);
     }
 
     public static Date getEndOfDay(Date date) {
@@ -64,15 +82,93 @@ public class TimeUtils {
         return calendar.getTime();
     }
 
-    public static Date getYesterday(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.add(Calendar.DATE, -1);
-        return calendar.getTime();
+    public static Date setTime(Date date, int field, int amount) {
+        Calendar c = Calendar.getInstance();
+        c.setLenient(false);
+        c.setTime(date);
+        c.set(field, amount);
+        return c.getTime();
     }
 
-    public static long dateDiff(Date from, Date to) {
-        return (to.getTime() - from.getTime()) / (24 * 60 * 60 * 1000);
+    public static Date setYear(Date date, int year) {
+        return setTime(date, Calendar.YEAR, year);
+    }
+
+    public static Date setMonth(Date date, int month) {
+        return setTime(date, Calendar.MONTH, month);
+    }
+
+    public static Date setDay(Date date, int day) {
+        return setTime(date, Calendar.DATE, day);
+    }
+
+    public static Date setHour(Date date, int hour) {
+        return setTime(date, Calendar.HOUR, hour);
+    }
+
+    public static Date setMinute(Date date, int minute) {
+        return setTime(date, Calendar.MINUTE, minute);
+    }
+
+    public static Date setSecond(Date date, int second) {
+        return setTime(date, Calendar.SECOND, second);
+    }
+
+    public static Date setMillisecond(Date date, int mSecond) {
+        return setTime(date, Calendar.MILLISECOND, mSecond);
+    }
+
+    public static Date getYesterday(Date date) {
+        return addDay(date, -1);
+    }
+
+    public static Date getTomorrow(Date date) {
+        return addDay(date, 1);
+    }
+
+    public static long getDateDiff(Date from, Date to) {
+        return (to.getTime() - from.getTime()) / MILLISECONDS_PER_DAY;
+    }
+
+    public static boolean isSameDay(Date date, Date comparedDate) {
+        return getDateDiff(date, comparedDate) == 0;
+    }
+
+    public static boolean isLeapYear(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    public static int getNoOfDaysInMonth(int year, int month) {
+        while(month > 12) {
+            month -= 12;
+            ++year;
+        }
+        while(month < 1) {
+            month += 12;
+            --year;
+        }
+        switch(month) {
+            case 2:
+                return isLeapYear(year) ? 29 : 28;
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            default:
+                return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+        }
+    }
+
+    public static int getNoOfDaysInMonth(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return getNoOfDaysInMonth(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1);
     }
 
     public static String getTimespan(Date startDate, Date endDate, Locale locale) {
@@ -85,8 +181,8 @@ public class TimeUtils {
         long year = duration.toDays() / 365;
 
         final ResourceBundle messages = ResourceBundle.getBundle("i18n/messages", locale);
-        if (year > 0) return year + " " + (year > 1 ? messages.getString("years_ago") :  messages.getString("year_ago"));
-        else if (week > 0) return week + " " + (week > 1 ?  messages.getString("weeks_ago") : messages.getString("week_ago"));
+        if (year > 0) return year + " " + (year > 1 ? messages.getString("years_ago") : messages.getString("year_ago"));
+        else if (week > 0) return week + " " + (week > 1 ? messages.getString("weeks_ago") : messages.getString("week_ago"));
         else if (day > 0) return day + " " + (day > 1 ? messages.getString("days_ago") : messages.getString("day_ago"));
         else if (hour > 0) return hour + " " + (hour > 1 ? messages.getString("hours_ago") : messages.getString("hour_ago"));
         else if (minute > 0) return minute + " " + (minute > 1 ? messages.getString("minutes_ago") : messages.getString("minute_ago"));
