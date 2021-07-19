9f704bb09071f64151742efb677817ff3a49eae5
dingnjat <annanshakai@gmail.com>
2021-07-19 23:11:18 +0700

    Update

--- EasyTaskCommon/src/main/java/com/eazitasc/util/EasyUtils.java
+++ EasyTaskCommon/src/main/java/com/eazitasc/util/EasyUtils.java
@@ -1,7 +1,7 @@
 package com.eazitasc.util;
 
 public class EasyUtils {
-    public static <T> T getValueOrElseNull(T orgValue, T altValue) {
+    public static <T> T getValueOrElseIfNull(T orgValue, T altValue) {
         return orgValue == null ? altValue : orgValue;
     }
 }
