aa8ab4ad118c36b52df6f34498ed32769ab0e64c
Larry <annanshakai@gmail.com>
2021-10-14 18:32:16 +0700

    Add some utils

---
+++ EasyTaskUtils/src/main/java/com/eazitasc/binding/paging/PagingResponse.java
@@ -0,0 +1,16 @@
+package com.eazitasc.binding.paging;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class PagingResponse<T> {
+    private int draw;
+    private int totalDraw;
+    private int recordsTotal;
+    private int recordsFiltered;
+    private List<T> data;
+}
