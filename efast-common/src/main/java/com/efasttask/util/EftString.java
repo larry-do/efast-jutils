bf20261352228e448328e176601f6a318a6f621f
Larry <annanshakai@gmail.com>
2021-11-23 17:00:22 +0700

    Update

--- efast-common/src/main/java/com/efasttask/util/EftString.java
+++ efast-common/src/main/java/com/efasttask/util/EftString.java
@@ -5,7 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Arrays;
 
 public class EftString {
-    private static final char[] SOURCE_VIETNAMESE_CHARACTERS = {'À', 'Á', 'Â', 'Ã', 'È', 'É',
+    private static final char[] SOURCE_CHARACTERS = {'À', 'Á', 'Â', 'Ã', 'È', 'É',
             'Ê', 'Ì', 'Í', 'Ò', 'Ó', 'Ô', 'Õ', 'Ù', 'Ú', 'Ý', 'à', 'á', 'â',
             'ã', 'è', 'é', 'ê', 'ì', 'í', 'ò', 'ó', 'ô', 'õ', 'ù', 'ú', 'ý',
             'Ă', 'ă', 'Đ', 'đ', 'Ĩ', 'ĩ', 'Ũ', 'ũ', 'Ơ', 'ơ', 'Ư', 'ư', 'Ạ',
@@ -17,7 +17,7 @@ public class EftString {
             'Ợ', 'ợ', 'Ụ', 'ụ', 'Ủ', 'ủ', 'Ứ', 'ứ', 'Ừ', 'ừ', 'Ử', 'ử', 'Ữ',
             'ữ', 'Ự', 'ự'};
 
-    private static final char[] DESTINATION_VIETNAMESE_CHARACTERS = {'A', 'A', 'A', 'A', 'E',
+    private static final char[] DESTINATION_CHARACTERS = {'A', 'A', 'A', 'A', 'E',
             'E', 'E', 'I', 'I', 'O', 'O', 'O', 'O', 'U', 'U', 'Y', 'a', 'a',
             'a', 'a', 'e', 'e', 'e', 'i', 'i', 'o', 'o', 'o', 'o', 'u', 'u',
             'y', 'A', 'a', 'D', 'd', 'I', 'i', 'U', 'u', 'O', 'o', 'U', 'u',
@@ -29,28 +29,28 @@ public class EftString {
             'o', 'O', 'o', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u',
             'U', 'u', 'U', 'u'};
 
-    public static char removeVietnameseAccent(char c) {
-        int index = Arrays.binarySearch(SOURCE_VIETNAMESE_CHARACTERS, c);
+    public static char convertToEnglishAlphabet(char c) {
+        int index = Arrays.binarySearch(SOURCE_CHARACTERS, c);
         if (index >= 0) {
-            c = DESTINATION_VIETNAMESE_CHARACTERS[index];
+            c = DESTINATION_CHARACTERS[index];
         }
         return c;
     }
 
-    public static String removeVietnameseAccent(final String str) {
+    public static String convertToEnglishAlphabet(final String str) {
         StringBuilder sb = new StringBuilder(str);
         for (int i = 0; i < sb.length(); i++) {
-            sb.setCharAt(i, EftString.removeVietnameseAccent(sb.charAt(i)));
+            sb.setCharAt(i, EftString.convertToEnglishAlphabet(sb.charAt(i)));
         }
         return sb.toString();
     }
 
-    public static String removeVietnameseAccent(final String str, final char link_char) {
-        StringBuilder sb = new StringBuilder(str);
+    public static String generateStandardizedCode(final String str, final char linkChar) {
+        StringBuilder sb = new StringBuilder(str.trim().toLowerCase());
         for (int i = 0; i < sb.length(); i++) {
-            sb.setCharAt(i, EftString.removeVietnameseAccent(sb.charAt(i)));
+            sb.setCharAt(i, EftString.convertToEnglishAlphabet(sb.charAt(i)));
         }
-        return sb.toString().replaceAll("\\s+", String.valueOf(link_char));
+        return sb.toString().replaceAll("\\s+", String.valueOf(linkChar));
     }
 
     public static String getExtension(String filename) {
@@ -58,5 +58,21 @@ public class EftString {
         if (filename == null) return null;
         int index = filename.lastIndexOf('.');
         return index >= 0 ? filename.substring(index) : "";
+    }
+
+    public static String standardize(String str) {
+        return capitalizePerWord(removeRedundantSpace(str));
+    }
+
+    public static String capitalizePerWord(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            result.append((i == 0 || Character.isWhitespace(str.charAt(i - 1)) ? Character.toUpperCase(str.charAt(i)) : str.charAt(i)));
+        }
+        return result.toString();
+    }
+
+    public static String removeRedundantSpace(String str) {
+        return str.trim().replaceAll("\\s+", " ");
     }
 }
