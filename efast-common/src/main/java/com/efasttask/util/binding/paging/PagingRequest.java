1af919260e51f29c2c868a6547f5a0580b853f17
Larry <annanshakai@gmail.com>
2021-12-12 17:13:48 +0700

    Remove Lombok

--- efast-common/src/main/java/com/efasttask/util/binding/paging/PagingRequest.java
+++ efast-common/src/main/java/com/efasttask/util/binding/paging/PagingRequest.java
@@ -1,14 +1,60 @@
 package com.efasttask.util.binding.paging;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class PagingRequest {
     private int draw;
     private int length;
     private int start;
     private Boolean desc;
     private String orderBy;
+
+    public PagingRequest() {
+    }
+
+    public PagingRequest(int draw, int length, int start, Boolean desc, String orderBy) {
+        this.draw = draw;
+        this.length = length;
+        this.start = start;
+        this.desc = desc;
+        this.orderBy = orderBy;
+    }
+
+    public int getDraw() {
+        return draw;
+    }
+
+    public void setDraw(int draw) {
+        this.draw = draw;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public Boolean getDesc() {
+        return desc;
+    }
+
+    public void setDesc(Boolean desc) {
+        this.desc = desc;
+    }
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
 }
--- efast-common/src/main/java/com/efasttask/util/binding/paging/PagingResponse.java
+++ efast-common/src/main/java/com/efasttask/util/binding/paging/PagingResponse.java
@@ -1,16 +1,62 @@
 package com.efasttask.util.binding.paging;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.List;
 
-@Getter
-@Setter
 public class PagingResponse<T> {
     private int draw;
     private int totalDraw;
     private int recordsTotal;
     private int recordsFiltered;
     private List<T> data;
+
+    public PagingResponse() {
+    }
+
+    public PagingResponse(int draw, int totalDraw, int recordsTotal, int recordsFiltered, List<T> data) {
+        this.draw = draw;
+        this.totalDraw = totalDraw;
+        this.recordsTotal = recordsTotal;
+        this.recordsFiltered = recordsFiltered;
+        this.data = data;
+    }
+
+    public int getDraw() {
+        return draw;
+    }
+
+    public void setDraw(int draw) {
+        this.draw = draw;
+    }
+
+    public int getTotalDraw() {
+        return totalDraw;
+    }
+
+    public void setTotalDraw(int totalDraw) {
+        this.totalDraw = totalDraw;
+    }
+
+    public int getRecordsTotal() {
+        return recordsTotal;
+    }
+
+    public void setRecordsTotal(int recordsTotal) {
+        this.recordsTotal = recordsTotal;
+    }
+
+    public int getRecordsFiltered() {
+        return recordsFiltered;
+    }
+
+    public void setRecordsFiltered(int recordsFiltered) {
+        this.recordsFiltered = recordsFiltered;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
 }
